ba772b39db8c53313c3a4a561f4c9cdbf0bbe866
akshaybhasme <akshaybhasme91@gmail.com>
2015-11-17 16:11:07 -0800

    login and register screen UI

--- eSKImo-Android/app/src/main/java/com/theavalanche/eskimo/RegisterActivity.java
+++ eSKImo-Android/app/src/main/java/com/theavalanche/eskimo/RegisterActivity.java
@@ -5,16 +5,16 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class LauncherActivity extends ActionBarActivity {
+public class RegisterActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launcher);
-        findViewById(R.id.tvRegister).setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_register);
+        findViewById(R.id.tvLogin).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LauncherActivity.this, RegisterActivity.class);
+                Intent intent = new Intent(RegisterActivity.this, LauncherActivity.class);
                 startActivity(intent);
                 finish();
             }
