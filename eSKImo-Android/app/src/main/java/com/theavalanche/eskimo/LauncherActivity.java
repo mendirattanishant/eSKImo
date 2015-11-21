4cac575283da6bd16f43285a285de1caaec84c97
Akshay Chandra <akshay.chandra23@gmail.com>
2015-11-21 02:40:04 -0800

    Checking Login Services

--- eSKImo-Android/app/src/main/java/com/theavalanche/eskimo/LauncherActivity.java
+++ eSKImo-Android/app/src/main/java/com/theavalanche/eskimo/LauncherActivity.java
@@ -11,7 +11,26 @@ public class LauncherActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        // Register
         findViewById(R.id.tvRegister).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LauncherActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        // Login with Facebook
+        findViewById(R.id.btn_facebook).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LauncherActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        // Login with Google
+        findViewById(R.id.btn_google).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LauncherActivity.this, RegisterActivity.class);
