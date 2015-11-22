a060b5786b1ddf63f36ea65ef5ec44e7b278518b
Akshay Chandra <akshay.chandra23@gmail.com>
2015-11-21 21:15:27 -0800

    Facbeook Login

--- eSKImo-Android/app/src/main/java/com/theavalanche/eskimo/LauncherActivity.java
+++ eSKImo-Android/app/src/main/java/com/theavalanche/eskimo/LauncherActivity.java
@@ -1,15 +1,35 @@
 package com.theavalanche.eskimo;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 public class LauncherActivity extends ActionBarActivity {
+
+    private CallbackManager callbackManager;
+    // private TextView info;
+    private LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_launcher);
         // Register
         findViewById(R.id.tvRegister).setOnClickListener(new View.OnClickListener() {
@@ -21,12 +41,25 @@ public class LauncherActivity extends ActionBarActivity {
             }
         });
         // Login with Facebook
-        findViewById(R.id.btn_facebook).setOnClickListener(new View.OnClickListener() {
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LauncherActivity.this, RegisterActivity.class);
-                startActivity(intent);
-                finish();
+            public void onSuccess(LoginResult loginResult) {
+               /* info.setText("User ID:  " +
+                        loginResult.getAccessToken().getUserId() + "\n" +
+                        "Auth Token: " + loginResult.getAccessToken().getToken());*/
+            }
+
+            @Override
+            public void onCancel() {
+                // info.setText("Login attempt cancelled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                // info.setText("Login attempt failed.");
             }
         });
         // Login with Google
@@ -38,5 +71,9 @@ public class LauncherActivity extends ActionBarActivity {
                 finish();
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
