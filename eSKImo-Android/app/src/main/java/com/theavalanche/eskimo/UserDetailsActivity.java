3a0148d44e35d6579263e1d78c7b7169298f028e
akshaybhasme <akshaybhasme91@gmail.com>
2015-11-24 13:40:54 -0800

    Adds user details screen with event list that the user is attending

--- eSKImo-Android/app/src/main/java/com/theavalanche/eskimo/UserDetailsActivity.java
+++ eSKImo-Android/app/src/main/java/com/theavalanche/eskimo/UserDetailsActivity.java
@@ -2,27 +2,27 @@ package com.theavalanche.eskimo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 
-public class EventDetailsActivity extends FragmentActivity {
+public class UserDetailsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event_details);
+        setContentView(R.layout.activity_user_details);
 
-        LinearLayout userListView = (LinearLayout) findViewById(R.id.llEventUsers);
+        LinearLayout userListView = (LinearLayout) findViewById(R.id.llEvents);
 
         LayoutInflater inflater = LayoutInflater.from(this);
         for(int i = 0; i < 10; i++){
-            View view =  inflater.inflate(R.layout.row_user, new LinearLayout(this));
+            View view =  inflater.inflate(R.layout.row_event, new LinearLayout(this));
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent intent = new Intent(EventDetailsActivity.this, UserDetailsActivity.class);
+                    Intent intent = new Intent(UserDetailsActivity.this, EventDetailsActivity.class);
                     startActivity(intent);
                 }
             });
